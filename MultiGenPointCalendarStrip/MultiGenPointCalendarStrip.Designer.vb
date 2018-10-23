fe7d8d6de188b9da35b3a18f42456d3b527788d1
Alessandro Mandelli <alessandro.mandelli@costergroup.eu>
2018-10-23 15:52:47 +0200

    Aggiungere i file di progetto.

---
+++ MultiGenPointCalendarStrip/MultiGenPointCalendarStrip.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MultiGenPointCalendarStrip
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1, 8)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'MultiGenPointCalendarStrip
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "MultiGenPointCalendarStrip"
+        Me.Size = New System.Drawing.Size(288, 8)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ MultiGenPointCalendarStrip/MultiGenPointCalendarStrip.vb
@@ -0,0 +1,67 @@
+﻿Public Class MultiGenPointCalendarStrip
+    Dim pbBoxes As PictureBox() = New PictureBox(287) {}
+    Private Sub New()
+        For i = 0 To 287
+            pbBoxes(i) = New PictureBox
+            With pbBoxes(i)
+                .Width = 1
+                .Height = 8
+                .Location = New Point(Me.Location.X + i, Me.Location.Y)
+                .BackColor = Color.Transparent
+                .Visible = True
+            End With
+            Me.Controls.Add(pbBoxes(i))
+        Next
+    End Sub
+    ''' <summary>
+    ''' Controllo numerico in combinazione tra trackbar e label
+    ''' </summary>
+    ''' <param name="F1On">Inizio prima fascia in multipli di 5 minuti (es.: 14 = 1h10')</param>
+    ''' <param name="F1Off">Fine prima fascia in multipli di 5 minuti (es.: 14 = 1h10')</param>
+    ''' <param name="F2On">Inizio seconda fascia in multipli di 5 minuti (es.: 14 = 1h10')</param>
+    ''' <param name="F2Off">Fine seconda fascia in multipli di 5 minuti (es.: 14 = 1h10')</param>
+    ''' <param name="F3On">Inizio terza fascia in multipli di 5 minuti (es.: 14 = 1h10')</param>
+    ''' <param name="F3Off">Fine quarta fascia in multipli di 5 minuti (es.: 14 = 1h10')</param>
+    ''' <remarks></remarks>
+
+    Public Sub Settings(ByVal F1On As Integer,
+                         F1Off As Integer,
+                         F2On As Integer,
+                         F2Off As Integer,
+                         F3On As Integer,
+                         F3Off As Integer)
+
+        If F1On > F1Off Or (F1On = 0 And F1Off = 0) Then
+            MsgBox("Fascia oraria F1 non impostata correttamente")
+            Exit Sub
+        End If
+        If F2On > F2Off Then
+            MsgBox("Fascia oraria F2 non impostata correttamente")
+            Exit Sub
+        End If
+        If F3On > F3Off Then
+            MsgBox("Fascia oraria F3 non impostata correttamente")
+            Exit Sub
+        End If
+
+        For i = 0 To 287
+            If i >= F1On And i <= F1Off Then
+                pbBoxes(i).BackColor = Color.Black
+            Else
+                pbBoxes(i).BackColor = Color.Transparent
+            End If
+            If i >= F2On And i <= F2Off Then
+                pbBoxes(i).BackColor = Color.Black
+            Else
+                pbBoxes(i).BackColor = Color.Transparent
+            End If
+            If i >= F3On And i <= F3Off Then
+                pbBoxes(i).BackColor = Color.Black
+            Else
+                pbBoxes(i).BackColor = Color.Transparent
+            End If
+        Next
+
+    End Sub
+
+End Class
